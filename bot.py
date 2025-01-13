bf066124cea0ceaa6b4b9a72f061fb41ca8afeca
Alekao18 <umzehonorio@gmail.com>
2025-01-13 20:57:37 -0300

    Criando a estrutura de mensagens

--- bot.py
+++ bot.py
@@ -6,27 +6,35 @@ KEY_API = '7642544580:AAFA_KXDWgP7jJukIg7WLiMYnJtQw8NsZMQ' #chave API
 bot = telebot.TeleBot(KEY_API)
 #conexão feita com o bot criado no telegram
 
+@bot.message_handler(commands=["1"]) #parametro que gera um comando ou função para a minha função rodar
+def digite1(mensagem):
+    bot.reply_to(mensagem, 'Obrigado por escolher nossa empresa')
+
+@bot.message_handler(commands=["2"])
+def digite2(mensagem):
+    reclamacao = bot.reply_to(mensagem, 'Digite aqui a sua reclamação:')
+    bot.register_next_step_handler(reclamacao, armazenar_reclamacao)
+
+def armazenar_reclamacao(mensagem): 
+    bot.reply_to(mensagem, 'Muito obrigado por nos informar, não haverá mais isso')
+
+@bot.message_handler(commands=["3"])
+def digite3(mensagem):
+    bot.reply_to(mensagem, 'Sessão encerrada')
+
 def verificacao(mensagem):
     return True #bot ira responder a qualquer mensagem
 #função que irá verificar se o usuario der inicio com o comando correto
 
 @bot.message_handler(func=verificacao) #parametro que gera um comando ou função para a minha função rodar
 def reposta(mensagem):
-    bot.reply_to(mensagem, "Olá, aqui é o LekoBot!") #mensagem padrão(teste)
-    texto = '''
-    Digite 1 para fazer um pedido
+    bot.reply_to(mensagem, 'Ola, eu sou o Leko')
+    texto = """ 
+     Digite 1 para fazer um pedido
     Digite 2 para reclamar de um pedido
     Digite 3 para encerrar
-    '''
-    bot.reply_to(texto)
-    if texto =='1':
-        bot.reply_to(mensagem,'Obrigado por escolher nossa empresa')
-    elif texto == '2':
-        reclamacao = bot.reply_to(mensagem,'Descreva aqui para nós o motivo de sua reclamação')
-        bot.reply_to(mensagem, reclamacao, 'Certo, pedimos perdão pelo transtorno e iremos informar assim que sair a procuração!')
-    else:
-        bot.reply_to(mensagem,'Volte sempre.')
+    """
+    bot.reply_to(mensagem, texto) #mensagem padrão(teste)
 #função para testar as respotas de mensagem do usuário
-
 
 bot.polling() #comando que permite a leitura de todas as mensagens recebidas pelo bot
