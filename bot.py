8b597d8d542fd5c4ce8052dc315c152863696a5a
Alekao18 <umzehonorio@gmail.com>
2025-01-13 20:15:11 -0300

    `Added new functionality to bot.py: user menu and conditional responses`

--- bot.py
+++ bot.py
@@ -13,6 +13,19 @@ def verificacao(mensagem):
 @bot.message_handler(func=verificacao) #parametro que gera um comando ou função para a minha função rodar
 def reposta(mensagem):
     bot.reply_to(mensagem, "Olá, aqui é o LekoBot!") #mensagem padrão(teste)
+    texto = '''
+    Digite 1 para fazer um pedido
+    Digite 2 para reclamar de um pedido
+    Digite 3 para encerrar
+    '''
+    bot.reply_to(texto)
+    if texto =='1':
+        bot.reply_to(mensagem,'Obrigado por escolher nossa empresa')
+    elif texto == '2':
+        reclamacao = bot.reply_to(mensagem,'Descreva aqui para nós o motivo de sua reclamação')
+        bot.reply_to(mensagem, reclamacao, 'Certo, pedimos perdão pelo transtorno e iremos informar assim que sair a procuração!')
+    else:
+        bot.reply_to(mensagem,'Volte sempre.')
 #função para testar as respotas de mensagem do usuário
 
 
